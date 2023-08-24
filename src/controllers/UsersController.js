31ee6f3b3f2c7f1fcd00830144a209029d4682bc
rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2023-08-23 23:54:25 -0300

    tratando error

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,14 @@
+const AppError = require('../utils/AppError')
+
 class UsersController {
     create(request, response) {
       const { name, email, password } = request.body;
+
+      if (!name) {
+        throw new AppError('Nome é obrigatório')
+      }
   
-      response.json({ name, email, password });
+      response.status(201).json({ name, email, password });
     }
 }
   
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,6 +4,7 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
+
 const userscontroller = new UsersController();
 
 usersRoutes.post('/', userscontroller.create);
