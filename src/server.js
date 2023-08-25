bccc649c3f7a4339d7de823e9752d9a2964d7aa6
rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2023-08-24 23:56:02 -0300

    manipulacao de dados

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 require("express-async-errors");
-
+const database = require("./database/sqlite");
 const AppError = require("./utils/AppError");
 const express = require('express');
 
-const routes = require("./routes")
+const routes = require("./routes");
 
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 app.use((error, request, response, next)=>{
     if(error instanceof AppError) {
