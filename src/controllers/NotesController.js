b01cf998621a68db58b682cfc69b1e57735bb5c8
rafaekokra <okra1002@gmail.com>
2023-09-27 23:06:18 -0300

    cadastro nota

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -34,6 +34,29 @@ class NotesController{
 
 
     }
-}
 
-module.exports = NotesController;
+    async show(request, response) {
+        const { id } = request.params;
+
+        const note = await knex("notes").where({ id }).first()
+        const tags = await knex("tags").where({ note_id: id }).orderBy("name")
+        const links = await knex("links").where({ note_id: id }).orderBy("created_at")
+    
+        return response.json({
+          ...note,
+          tags,
+          links
+        });
+      }
+
+    async delete(request, reponse){
+        const { id } = request.params;
+
+        await knex("notes").where({ id }).delete();
+
+        return responde.json();
+        }
+
+    }
+    
+    module.exports = NotesController
