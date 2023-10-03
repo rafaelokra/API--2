41d29e516fc4d093640c642e5209c42c6584c7d0
rafaekokra <okra1002@gmail.com>
2023-10-03 00:01:10 -0300

    Filtro WhereIn

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -30,7 +30,7 @@ class NotesController {
 
     await knex("tags").insert(tagsInsert)
 
-    response.json();
+    response.json()
   }
 
   async show(request, response) {
@@ -56,16 +56,25 @@ class NotesController {
   }
 
   async index(request, response) {
-    const {  title, user_id } = request.query;
+    const { title, user_id, tags } = request.query
 
-    const notes = await knex("notes")
-    .where({ user_id})
-    .whereLike("title", `%${title}%`)
-    .orderBy("title")
+    let notes
 
-    return response.json(notes);
-    
-  } 
+    if (tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim())
+
+      notes = await knex("tags")
+        .whereIn("name", filterTags)
+        
+    } else {
+      notes = await knex("notes")
+      .where({ user_id })
+      .whereLike("title", `%${title}%`)
+      .orderBy("title")
+    }
+
+    return response.json(notes)
+  }
 }
 
 module.exports = NotesController
