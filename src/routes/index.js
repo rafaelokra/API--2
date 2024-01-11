abb0874fdfd2d959eea2bdbbed8f5abcbdd2d8da
Rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2024-01-10 23:13:40 -0300

    SessionsController

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require('express')
 const usersRoutes = require('./users.routes')
 const notesRoutes = require('./notes.routes')
 const tagsRoutes = require("./tags.routes")
+const sessionsRouter = require("./Sessions.routes")
 
 const routes = Router()
 routes.use('/users', usersRoutes)
 routes.use('/notes', notesRoutes)
 routes.use('/tags', tagsRoutes)
+//routes.use('/sessions', sessionsRouter)
 
 module.exports = routes
